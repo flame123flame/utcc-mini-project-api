66251908318f56ceea4892fbf32d70e34ac3c539
flame123flame <flame.tana@gmail.com>
2023-08-26 15:33:29 +0700

    add search

--- src/main/java/UTCC/project/work/controller/BusTerminalController.java
+++ src/main/java/UTCC/project/work/controller/BusTerminalController.java
@@ -1,5 +1,41 @@
 package UTCC.project.work.controller;
 
-public class BusTerminalController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import UTCC.framework.constant.ResponseConstant.RESPONSE_MESSAGE;
+import UTCC.framework.constant.ResponseConstant.RESPONSE_STATUS;
+import UTCC.framework.model.ResponseData;
+import UTCC.project.work.model.BusTerminal;
+import UTCC.project.work.service.BusTerminalService;
+
+
+@RestController
+@RequestMapping("api/bus-terminal/")
+public class BusTerminalController {
+		
+	
+	@Autowired
+	private BusTerminalService busTerminalService;
+	
+	@GetMapping("search")
+	public ResponseData<List<BusTerminal>> search( ) {
+		ResponseData<List<BusTerminal>> responseData = new ResponseData<>();
+		try {
+			responseData.setData(busTerminalService.getBusTerminal());
+			responseData.setMessage(RESPONSE_MESSAGE.GET.SUCCESS);
+			responseData.setStatus(RESPONSE_STATUS.SUCCESS);
+		} catch (Exception e) {
+			responseData.setMessage(RESPONSE_MESSAGE.GET.FAILED);
+			responseData.setStatus(RESPONSE_STATUS.FAILED);
+		}
+		return responseData;
+	}
+	
+	
+	
 }

fab84384679f3b38f0bac0c0202279c795c337e7
yongangke <sippakorn.9900@email.com>
2023-09-02 20:34:46 +0700

    fix BusDivision missiong

---
+++ src/main/java/UTCC/project/work/model/BusDivision.java
@@ -0,0 +1,52 @@
+package UTCC.project.work.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "bus_division")
+public class BusDivision {
+
+	private static final long serialVersionUID = -839822370605016277L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bus_division_id")
+    private long busDivisionId;
+
+    @Column(name = "bus_division_no")
+    private long busDivisionNo;
+
+    @Column(name = "bus_division_name")
+    private String busDivisionName;
+
+    @Column(name = "bmta_zone")
+    private long busZone;
+    
+    @Column(name = "bus_depot_id")
+    private long busDepotId;
+    
+    @Column(name = "create_by", length = 50, nullable = false)
+    private String createBy;
+
+    @Column(name = "update_by", length = 50, nullable = false)
+    private String updateBy;
+
+    @Column(name = "create_date", length = 6, nullable = false)
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    @Column(name = "update_date", length = 6, nullable = false)
+    private LocalDateTime updateDate = LocalDateTime.now();
+
+}

d64e55dde8c40d46f05e1e2c08c1e9eaec7555c7
yongangke <sippakorn.9900@email.com>
2023-10-13 12:17:08 +0700

    Add Model&Repo TicketTrip

---
+++ src/main/java/UTCC/project/work/model/TicketTrip.java
@@ -0,0 +1,53 @@
+package UTCC.project.work.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket_trip")
+public class TicketTrip implements Serializable{
+	
+	private static final long serialVersionUID = 7291715488206045455L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ticket_trip_id")
+    private long ticketTripId;
+    
+	@Column(name = "worksheet_id")
+    private long worksheetId;
+	
+	@Column(name = "trip")
+    private long trip;
+    
+    @Column(name = "ticket_begin")
+    private Boolean ticketBegin;
+    
+    @Column(name = "ticket_end")
+    private Boolean ticketEnd;
+    
+    @Column(name = "create_by", length = 50,nullable = false)
+    private String createBy;
+
+    @Column(name = "update_by", length = 50,nullable = false)
+    private String updateBy;
+
+    @Column(name = "create_date", length = 6, nullable = false)
+    private LocalDateTime createDate;
+
+    @Column(name = "update_date", length = 6, nullable = false)
+    private LocalDateTime updateDate;
+
+}
---
+++ src/main/java/UTCC/project/work/repositories/TicketTripRepository.java
@@ -0,0 +1,9 @@
+package UTCC.project.work.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import UTCC.project.work.model.TicketTrip;
+
+public interface TicketTripRepository extends CrudRepository<TicketTrip, Long>{
+
+}
